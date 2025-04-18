80e19b8060e101b984dc5b80b9f38c2e1ef393bd
Agbebi Bidemi <agbebibidemi02@gmail.com>
2025-04-18 14:57:38 +0100

    configuring schamp

--- server/api/send-sms.post.js
+++ server/api/send-sms.post.js
@@ -19,7 +19,7 @@ export default defineEventHandler(async (event) => {
     const response = await axios.post('https://api.sendchamp.com/api/v1/sms/send', {
       to: [body.to],
       message: body.message,
-      sender_name: body.sender || 'Sendchamp',
+      sender_name: body.sender || 'Schamp',
       route: body.route || 'non_dnd',
     }, {
       headers: {
